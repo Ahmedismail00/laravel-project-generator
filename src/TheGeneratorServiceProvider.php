43c873f813b5519256be02904a16572c3e5e0ae5
ahmedismail00 <ahmed.ismail11199@gmail.com>
2023-07-26 10:01:28 +0300

    publish commands

---
+++ src/TheGeneratorServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Console\Commands\InitGenerator;
+use App\Console\Commands\LaunchGenerator;
+use App\Console\Commands\MakeGeneratorController;
+use Illuminate\Support\ServiceProvider;
+
+class TheGeneratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     */
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                InitGenerator::class,
+                LaunchGenerator::class,
+                MakeGeneratorController::class,
+                MakeGeneratorMigration::class,
+                MakeGeneratorModel::class,
+                MakeGeneratorRequest::class,
+            ]);
+        }
+    }
+}
